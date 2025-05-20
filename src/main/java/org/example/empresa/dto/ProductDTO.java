6eb14c30ccbaa002c5763f5dc2fcb09d0ced9c02
クリス <chris.syst3@gmail.com>
2025-05-20 10:17:41 -0600

    Se hacen cambios para la clase de qa impartida segun la practica de bugs reportados
    y se generan otros bugs para pruebas de regresion

--- src/main/java/org/example/empresa/dto/ProductDTO.java
+++ src/main/java/org/example/empresa/dto/ProductDTO.java
@@ -1,7 +1,9 @@
 package org.example.empresa.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,11 +17,12 @@ public class ProductDTO {
     @NotNull(message = "id is required")
     private Long id;
 
-    /*@NotBlank(message = "title is required")
-    @NotNull(message = "title is required")*/
+    @NotBlank(message = "title is required")
+    @NotNull(message = "title is required")
     private String title;
 
-    // @NotNull(message = "price is required")
+    @NotNull(message = "price is required")
+    @Min(value = 0, message = "price must be greater than or equal to 0")
     private Double price;
 
     @NotNull(message = "description is required")
@@ -28,6 +31,7 @@ public class ProductDTO {
 
     @NotNull(message = "category is required")
     @NotBlank(message = "category is required")
+    @Pattern(regexp="^(ENTRETENIMIENTO|CINE|HOGAR)$",message="description must be one of the following values: ENTREGA, CINE, HOGAR")
     private String category;
 
     @NotNull(message = "image is required")
